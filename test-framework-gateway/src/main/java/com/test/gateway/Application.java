fe7278d8699e934d1849235982056544b5d55d3c
god <naughty7878@163.com>
2025-05-30 19:42:53 +0800

    版本升级，使用SDK17

--- test-framework-gateway/src/main/java/com/test/gateway/Application.java
+++ test-framework-gateway/src/main/java/com/test/gateway/Application.java
@@ -2,10 +2,10 @@ package com.test.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
